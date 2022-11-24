96f8480fc57758b79fe27cc80bc42f79a16623c2
DESKTOP-4RQUT1S\PC-13 <dnjstjsthd@gmail.com>
2022-11-24 11:01:06 +0900

    modified:   src/App.css 	modified:   src/App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
 
+// 함수형 컴포넌트 App
 function App() {
-  return (
+  let str = "내가 해야할 일";
+  let cnm = "hStyle";
+  //태그로 이용하는것
+  let imgStyle ={width: '200px', height:'200px;', borderRadius:'5px', border: '1px solid #4545'};
+
+  function btn(){
+    return "클릭버튼";
+  }
+
+  return(
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="black-nav">
+        <div>Todo 리스트</div>
+      </div>
+      <h4 className={cnm}>{str}</h4>
+      <img src={roomhaha} style={imgStyle} />
+      {/* <img src={logo} style={{width: '200px', height: '200px;', borderRadius: '5px', border: '1px solid #4545'}} /> */}
     </div>
   );
+  
 }
-
+//내보내는 역할 export defualt App : => 변수나 함수 를 내보낼수있음
 export default App;

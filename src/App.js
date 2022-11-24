6eb84734bd6ddde6cce441e978b1084e2e536973
GomWS <dnjstjsthd@gmail.com>
2022-11-24 20:08:05 +0900

    modified:   src/App.css 	modified:   src/App.js 	new file:   src/img/room.jpg

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
+function App(){
+  let a = [1,2]; // eslint-disable-line no-unused-vars
+  let [i1, i2=7, i3=5] = a; // eslint-disable-line no-unused-vars
+
+  return(
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <div className="black-nav">
+         <div>구조분해할당</div>
+       </div><br />
+       i1: {i1}<br />
     </div>
   );
 }
-
 export default App;

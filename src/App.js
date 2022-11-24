4bdf9b7c589f4ca64ff801b6dc042666c45c7e61
DESKTOP-4RQUT1S\PC-13 <dnjstjsthd@gmail.com>
2022-11-24 13:13:26 +0900

    modified:   src/App.js

--- src/App.js
+++ src/App.js
@@ -149,7 +149,7 @@ function App(){
   let addstr = 'Id';
   let obj2 = {['one'+ addstr] : '키 이름 변경하기'};
   let obj3 = {
-    obj2 : obj2,
+    obj2,
     md1 : function(a,b){return `md1메소드 실행: ${a+b}`;},
     md2(){return "md2메소드 실행";}
   };
@@ -167,7 +167,7 @@ function App(){
       obj2객체의 요소 값: {obj2['one' + addstr]}<br />
       obj3객체의 md1(): {obj3.md1(5,3)}<br />
       obj3객체의 md2(): {obj3.md2()}<br />
-      obj3객체의 obj2객체의 요소 값: {obj3.obj2['one' + addstr]}<br />
+      obj3객체의 obj2객체의 요소 값: {obj3.obj2['one'+addstr]}<br />
     </div>
   );
 }
